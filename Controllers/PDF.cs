06fa94a2440fe6c01a142b7dd545236c95b67cc0
Astrof123 <valeev_101@bk.ru>
2024-10-27 12:01:29 +0800

    Initialization + added function of uploading files

---
+++ Controllers/PDF.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PDF_API.Models;
+
+namespace PDF_API.Controllers {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PDF : ControllerBase {
+
+        [HttpPost]
+        //[DisableCors]
+        public IActionResult Negr(IFormFile file) {
+            return Ok(new UploadHandler().Upload(file));
+        }
+    }
+}
---
+++ Models/UploadHandler.cs
@@ -0,0 +1,30 @@
+﻿namespace PDF_API.Models {
+    public class UploadHandler {
+
+        public string Upload(IFormFile file) {
+            List<string> validExtensions = new List<string>() { ".pdf" };
+
+            string extention = Path.GetExtension(file.FileName);
+
+            if (!validExtensions.Contains(extention)) {
+                return "Extension is not valid";
+            }
+
+            long size = file.Length;
+
+            if (size > (100 * 1024 * 1024)) {
+                return "Maximum size can be 100Mb";
+            }
+
+            string fileName = Guid.NewGuid().ToString() + extention;
+
+            string path = Path.Combine(Directory.GetCurrentDirectory(), "Uploads");
+
+            using FileStream stream = new FileStream(Path.Combine(path, fileName), FileMode.Create);
+
+            file.CopyTo(stream);
+
+            return fileName;
+        }
+    }
+}
